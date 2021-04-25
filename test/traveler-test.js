26fc27b0c89cf0c1a72f0bd7898f4fff443cbde7
Ashish Malla <ashishmalla45@gmail.com>
2021-04-25 17:31:19 -0600

    Add test to traveler class properties

---
+++ test/traveler-test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import { allUsersData, allTripsData, allDestinationData } from '../src/data/sampleTestData';
+import Traveler from '../src/traveler';
+import Trip from '../src/trips';
+
+describe('Trip', () => {
+  let traveler;
+  beforeEach(() => {
+    traveler = new Traveler(allUsersData[3])
+  });
+
+  describe('Traveler properties', () => {
+    it('should be a function', () => {
+      expect(Traveler).to.be.a('function')
+    })
+
+    it('should be an instance of Trip', () => {
+      expect(traveler).to.be.an.instanceof(Traveler)
+    })
+
+    it('should have an id', () => {
+      expect(traveler.id).to.equal(44)
+      traveler = new Traveler(allUsersData[2])
+      expect(traveler.id).to.equal(32)
+    })
+
+    it('should have a name', () => {
+      expect(traveler.name).to.equal('Marijo MacNeilley')
+      traveler = new Traveler(allUsersData[2])
+      expect(traveler.name).to.equal('Karlee Dowsey')
+    })
+
+    it('should have a type', () => {
+      expect(traveler.travelerType).to.equal('photographer')
+      traveler = new Traveler(allUsersData[2])
+      expect(traveler.travelerType).to.equal('thrill-seeker')
+    })
+
+    it('should have a date', () => {
+      expect(traveler.currentDate).to.equal(1593583200000)
+    })
+
+    it('should have no data by default', ()=> {
+      expect(traveler.allTrips).to.deep.equal([])
+      expect(traveler.myDestinations).to.deep.equal([])
+      expect(traveler.past).to.deep.equal([])
+      expect(traveler.present).to.deep.equal([])
+      expect(traveler.upcoming).to.deep.equal([])
+      expect(traveler.pending).to.deep.equal([])
+    })
+  })
+  
+ 
+})
